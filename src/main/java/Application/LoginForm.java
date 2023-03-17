412119693f36a2f1092b9c44384de2bed11d0512
minhtrungle <minhtrungle21599@gmail.com>
2023-03-17 15:57:52 +0700

    Update Swing

---
+++ src/main/java/Application/LoginForm.java
@@ -0,0 +1,10 @@
+package Application;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JTextField textUser;
+    private JTextField textPassword;
+    private JButton btnOK;
+    private JButton btnCancel;
+}
