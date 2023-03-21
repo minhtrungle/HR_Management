81c14bc84d60e7a9ff38ef65e299a62e85052dc3
minhtrungle <minhtrungle21599@gmail.com>
2023-03-21 16:15:50 +0700

    Update DeleteDepartment

---
+++ src/main/java/Application/Department/DeleteDepartment.java
@@ -0,0 +1,11 @@
+package Application.Department;
+
+import javax.swing.*;
+
+public class DeleteDepartment {
+    public JPanel getDeleteDepartmentPanel() {
+        return deleteDepartmentPanel;
+    }
+    private JPanel deleteDepartmentPanel;
+    private JTextField textField1;
+}
