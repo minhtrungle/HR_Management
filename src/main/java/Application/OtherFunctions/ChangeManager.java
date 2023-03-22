91c9fe2b3c4d885805ba1a17c102f4825a6728bf
minhtrungle <minhtrungle21599@gmail.com>
2023-03-22 12:06:38 +0700

    Update

---
+++ src/main/java/Application/OtherFunctions/ChangeManager.java
@@ -0,0 +1,10 @@
+package Application.OtherFunctions;
+
+import javax.swing.*;
+
+public class ChangeManager {
+    public JPanel getChangeManagerPanel() {
+        return changeManagerPanel;
+    }
+    private JPanel changeManagerPanel;
+}
