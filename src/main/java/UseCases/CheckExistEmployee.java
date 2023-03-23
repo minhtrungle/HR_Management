41cd2fbd70948f8a68ece941cdcfaa67867992b5
minhtrungle <minhtrungle21599@gmail.com>
2023-03-24 01:06:08 +0700

    Update Check

---
+++ src/main/java/UseCases/CheckExistEmployee.java
@@ -0,0 +1,17 @@
+package UseCases;
+
+import Dao.EmployeeDAO;
+import Model.Employee;
+
+import java.sql.SQLException;
+
+public class CheckExistEmployee {
+    public boolean checkExistEmployeeID(int id) throws SQLException {
+        EmployeeDAO empDAO = new EmployeeDAO();
+        Employee emp = empDAO.getByID(id);
+        if (emp != null) {
+            return true;
+        }
+        return false;
+    }
+}
