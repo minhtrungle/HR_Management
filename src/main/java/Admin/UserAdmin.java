6b6970cd4f0aea8f5e89af47f81f4dd41353507b
minhtrungle <minhtrungle21599@gmail.com>
2023-03-28 12:05:55 +0700

    Update User

---
+++ src/main/java/Admin/UserAdmin.java
@@ -0,0 +1,124 @@
+package Admin;
+
+import Dao.UserDAO;
+import Model.User;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
+
+public class UserAdmin {
+    private UserDAO userDAO = new UserDAO();
+    private User user = new User();
+    public UserAdmin() {
+            addButton.addActionListener(e -> {
+                String HRUsername = textHRUsername.getText();
+                String HRPassword = String.valueOf(textHRPassword.getPassword());
+                String username = textNewUsername.getText();
+                String password = String.valueOf(textNewPassword.getText());
+
+                if (Objects.equals(HRUsername, "trunglm") && Objects.equals(HRPassword, "trunglm")) {
+                    try {
+                        user.setUsername(username);
+                        user.setPassword(password);
+
+                        userDAO.insertUser(user);
+
+                        JOptionPane.showMessageDialog(null,
+                                "Thêm user thành công",
+                                "Thông báo",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        //Đóng
+                        JComponent component = (JComponent) e.getSource();
+                        Window window = SwingUtilities.getWindowAncestor(component);
+                        window.dispose();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Chỉ có HR Manager mới có thể sử dụng tác vụ này",
+                            "Thông báo",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            });
+            updateButton.addActionListener(e -> {
+                String HRUsername = textHRUsername.getText();
+                String HRPassword = String.valueOf(textHRPassword.getPassword());
+                String userName = textNewUsername.getText();
+                String passWord = String.valueOf(textNewPassword.getText());
+
+                if (Objects.equals(HRUsername, "trunglm") && Objects.equals(HRPassword, "trunglm")) {
+                    try {
+                        user.setPassword(passWord);
+
+                        userDAO.updatePasswordUser(user, userName);
+
+                        JOptionPane.showMessageDialog(null,
+                                "Đổi mật khẩu user thành công",
+                                "Thông báo",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        //Đóng
+                        JComponent component = (JComponent) e.getSource();
+                        Window window = SwingUtilities.getWindowAncestor(component);
+                        window.dispose();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Chỉ có HR Manager mới có thể sử dụng tác vụ này",
+                            "Thông báo",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            });
+            deleteButton.addActionListener(e -> {
+                String HRUsername = textHRUsername.getText();
+                String HRPassword = String.valueOf(textHRPassword.getPassword());
+                String userName = textNewUsername.getText();
+
+                if (Objects.equals(HRUsername, "trunglm") && Objects.equals(HRPassword, "trunglm")) {
+                    try {
+                        userDAO.deleteUser(userName);
+
+                        JOptionPane.showMessageDialog(null,
+                                "Xóa user thành công",
+                                "Thông báo",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        //Đóng
+                        JComponent component = (JComponent) e.getSource();
+                        Window window = SwingUtilities.getWindowAncestor(component);
+                        window.dispose();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null,
+                            "Chỉ có HR Manager mới có thể sử dụng tác vụ này",
+                            "Thông báo",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            });
+            cancelButton.addActionListener(e -> {
+                JComponent component = (JComponent) e.getSource();
+                Window window = SwingUtilities.getWindowAncestor(component);
+                window.dispose();
+            });
+        }
+
+
+
+    public JPanel getUserPanel() {
+        return userPanel;
+    }
+    private JPanel userPanel;
+    private JTextField textHRUsername;
+    private JPasswordField textHRPassword;
+    private JTextField textNewUsername;
+    private JTextField textNewPassword;
+    private JButton cancelButton;
+    private JButton deleteButton;
+    private JButton addButton;
+    private JButton updateButton;
+}
--- src/main/java/Dao/UserDAO.java
+++ src/main/java/Dao/UserDAO.java
@@ -1,13 +1,11 @@
 package Dao;
 
 import Connection.ConnectJDBC;
+import Model.Department;
 import Model.Employee;
 import Model.User;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UserDAO {
     public User getById(long id) {
@@ -53,10 +51,29 @@ public class UserDAO {
 
         return user;
     }
+    public User getByUserName(String username) {
+        User user = null;
+        try {
+            Connection conn = ConnectJDBC.getConnection();
+            String sql = String.format("SELECT id, username FROM users WHERE username='%s' LIMIT 1 ",
+                    username);
+
+            Statement stmt = conn.createStatement();
+            ResultSet resultSet = stmt.executeQuery(sql);
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setUsername(resultSet.getString("username"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
     public void insertUser(User u) throws SQLException {
         Connection con = ConnectJDBC.getConnection();
-        final String sql = String.format("INSERT INTO `users` VALUES ('%s', '%s')", u.getUsername(), u.getPassword());
-
+        final String sql = String.format("INSERT INTO `users` VALUES (NULL, '%s', '%s')", u.getUsername(), u.getPassword());
 
         try {
             Statement sta = con.createStatement();
@@ -65,6 +82,60 @@ public class UserDAO {
 
             if (res == 0) {
                 System.out.println("Insert users thất bại");
+            }
+
+            sta.close();
+            con.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void updatePasswordUser(User u, String username) throws SQLException {
+        Connection con = ConnectJDBC.getConnection();
+
+        User user = getByUserName(username);
+        if (user == null) {
+            throw new RuntimeException("Không có user thõa mãn!");
+        }
+
+        final String sql = String. format("UPDATE `users` SET `password` = '%s' WHERE username = '" +username+ "'",
+                u.getPassword(), username);
+
+        try {
+            Statement sta = con.createStatement();
+
+            long res = sta.executeUpdate(sql);
+
+            if (res == 0) {
+                System.out.println("Update users thất bại");
+            }
+
+            sta.close();
+            con.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void deleteUser(String username) throws SQLException {
+        Connection con = ConnectJDBC.getConnection();
+
+        User user = getByUserName(username);
+        if (user == null) {
+            throw new RuntimeException("Không có phòng ban thõa mãn!");
+        }
+
+        final String sql = "DELETE FROM `users` WHERE username = '" +username+ "'";
+
+        try {
+            Statement sta = con.createStatement();
+
+            long res = sta.executeUpdate(sql);
+
+            if (res == 0) {
+                System.out.println("Delete users thất bại");
             }
 
             sta.close();
